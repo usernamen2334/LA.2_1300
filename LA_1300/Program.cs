333c2a8010242161eeb9fb0965111ea7f6b8ba56
Erik Marku <110892575+usernamen2334@users.noreply.github.com>
2023-09-14 14:10:43 +0200

    Create Program.cs

---
+++ LA_1300/Program.cs
@@ -0,0 +1,11 @@
+namespace LA_2._1300
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GameMenu mainMenu = new GameMenu();
+            mainMenu.Start();
+        }
+    }
+}
